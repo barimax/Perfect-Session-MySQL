1d84bae852d89596d32aa2f789326cb79ee7c1e6
barimax <georgie@barimax.bg>
2019-11-27 11:02:42 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 		.library(name: "PerfectSessionMySQL", targets: ["PerfectSessionMySQL"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/barimax/Perfect-Session.git", "4.0.0"..<"4.1.0"),
+		.package(url: "https://github.com/barimax/Perfect-Session.git", .branch("master")),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", "3.0.0"..<"4.0.0"),
 	],
 	targets: [
